5439ce2d54e872f50ea18a179960e1a01c8d6016
Muhammad Ahmad Sultan <ahmadsultan3.0@outlook.com>
2025-06-25 22:49:51 +0500

    Create Account API created, JWT enabled, MongoDB Connection Established

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,16 @@
+
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const express = require("express");
 const cors = require("cors");
+
+const user = require("./models/user.model");
+
+mongoose.connect(config.connectionString);
+
 
 const jwt = require("jsonwebtoken");
 
@@ -8,12 +18,48 @@ const app = express();
 app.use(express.json());
 app.use(cors({origin: "*" }));
 
-// Testing the API
-app.get("/hello", async (req, res) => {
-    return res
-     .status(200)
-     .json({message: "hello"});
-})
+// Create Account - SignUp
+app.get("/create-account", async (req, res) => {
+    const {fullName, email, password } = req.body;
+
+    if (!fullName || !email || !password) {
+        return res
+            .status(400)
+            .json({ error: true, message: "All fields are required."});
+    }
+
+    const isUser = await UserActivation.findOne({email});
+    if (isUser){
+        return res
+            .status(400)
+            .json({ error: true, message: "User already exists."});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+        fullName,
+        email,
+        password: hashedPassword,
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign(
+        { userId: user._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "72h",
+        }
+    );
+
+    return res.status(201).json({
+        error: false,
+        user: {fullName: user.fullName, email: user.email },
+        accessToken,
+        message: "Registration Successful",
+    });
+});
 
 
 
