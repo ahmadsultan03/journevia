1f833c5cc281fb337d2bba0fcf5b851165f9ca66
Muhammad Ahmad Sultan <ahmadsultan3.0@outlook.com>
2025-07-23 19:21:33 +0500

    MongoDB Initial Setup Completed

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB connected successfully!');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+
+})
 
 const app = express();
 
