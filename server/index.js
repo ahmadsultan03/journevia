5a3d0178e5ebbbc60c1c7404ea31373141023371
Ahmad Sultan <m.ahmad.sultan82@gmail.com>
2025-07-25 18:27:19 +0500

    SignUp API Route Created

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -16,8 +18,13 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const app = express();
 
+// For allowing JSON data in requests
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!')
 });
+
+app.use("/api/auth", authRoutes)
 
 
